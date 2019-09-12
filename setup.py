926a9e1436a2e35ed4cfbdddb135638efeb868ed
Robert Dyer <rdyer@bgsu.edu>
2019-09-11 20:44:20 -0400

    add package setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="boa-api",
+    version="0.1.0",
+    author="Boa Project",
+    author_email="boasupport@iastate.edu",
+    description="Client API for using the Boa infrastructure",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/boalang/api-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
