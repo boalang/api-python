8da603917e8df7a1b97be0eabf341efa8f8a7004
Robert Dyer <rdyer@unl.edu>
2022-07-07 10:56:00 -0500

    release 0.1.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="boa-api",
-    version="0.1.12",
+    version="0.1.13",
     author="Boa Project",
     author_email="boasupport@iastate.edu",
     description="Client API for using the Boa infrastructure",
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/boalang/api-python",
     packages=setuptools.find_packages(),
-    package_data={package: ["py.typed", ".pyi", "**/.pyi"] for package in setuptools.find_packages()},
+    package_data={package: ["py.typed", "*.pyi", "**/*.pyi"] for package in setuptools.find_packages()},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
