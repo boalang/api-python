3ec2d94ed55e374091b472c7d4020ce232802892
Robert Dyer <rdyer@unl.edu>
2022-07-07 10:54:52 -0500

    fix typo

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/boalang/api-python",
     packages=setuptools.find_packages(),
-    package_data={package: ["py.typed", ".pyi", "**/.pyi"] for package in setuptools.find_packages()}
+    package_data={package: ["py.typed", ".pyi", "**/.pyi"] for package in setuptools.find_packages()},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
